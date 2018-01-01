2adc438a1711e94333bef08bc5eab5b99ede89a5
chois9105 <chois9105@gmail.com>
2018-01-01 11:52:35 +0900

    Init

---
+++ src/main/java/io/github/chois9105/springboot/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package io.github.chois9105.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
